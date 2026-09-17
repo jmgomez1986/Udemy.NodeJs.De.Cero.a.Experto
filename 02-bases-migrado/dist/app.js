d8b5b37491fb6d71c354a7fa1946fb0983adecd1
Matias Gomez <jmgametal@gmail.com>
2026-09-17 09:23:26 -0300

    Se eliminan node_modules

--- 02-bases-migrado/dist/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// import { buildLogger } from "./plugins/logger.plugin";
-// const logger = buildLogger("app.js");
-// logger.log("Hola mundo");
-// logger.error("Esto es algo malo");
